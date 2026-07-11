e720ee1f729174ffe8806ecb9e372d1b46746194
ancomfortsofas-cmd <ancomfortsofas@gmail.com>
2026-07-11 18:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ ${message}`;
         "_blank"
     );
 }
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+setInterval(()=>{
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current>=slides.length){
+current=0;
+}
+
+slides[current].classList.add("active");
+
+},3000);
